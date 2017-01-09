16b886ba803e8454132570fa749bc2347c2dda08
Ashok Kumar M <samratashokkumar@gmail.com>
2017-01-09 09:28:26 +0530

    Update androidexport.sh

--- androidexport.sh
+++ androidexport.sh
@@ -3,34 +3,34 @@ then
   echo "Usage: $0 <svg File> <Prefix> <MDPI Width> <MDPI Height>"
 elif [ ${1#*.} != "svg" ]
 then
-	echo "Provide only svg file"
+        echo "Provide only svg file"
 elif ! echo $3 | egrep -q '^[0-9]+$' ;
 then
-	echo "Provide width as number"
+        echo "Provide width as number"
 elif ! echo $4 | egrep -q '^[0-9]+$' ;
 then
-	echo "Provide height as number"
+        echo "Provide height as number"
 else
-	baseWidth=`expr $3 / 4`
-	baseHeight=`expr $4 / 4`
-	filename=$2"_"${1%.*}".png"
-	ratios=(3 4 6 8)
-	folders=(ldpi mdpi hdpi xhdpi)
-	index=0
-	for ratio in ${ratios[@]}
-	do
-		foldername=drawable-${folders[$index]}
-		exportfile=$foldername/$filename
-		width=`expr $baseWidth \* $ratio`
-		height=`expr $baseHeight \* $ratio`
+        baseWidth=$(bc -l <<< "$3 / 4")
+        baseHeight=$(bc -l <<< "$4 / 4")
+        filename=$2"_"${1%.*}".png"
+        ratios=(4 6 8 12)
+        folders=(mdpi hdpi xhdpi xxhdpi)
+        index=0
+        for ratio in ${ratios[@]}
+        do
+                foldername=drawable-${folders[$index]}
+                exportfile=$foldername/$filename
+                width=$(bc -l <<< "$baseWidth * $ratio")
+                height=$(bc -l <<< "$baseHeight * $ratio")
 
-		if [ ! -d $foldername ]
-		then
-			mkdir $foldername
-		fi
+                if [ ! -d $foldername ]
+                then
+                        mkdir $foldername
+                fi
 
-		inkscape -f $1 -e $exportfile -w $width -h $height
+                inkscape -f $1 -e $exportfile -w $width -h $height
 
-		index=`expr $index + 1`
-	done
+                index=`expr $index + 1`
+        done
 fi
